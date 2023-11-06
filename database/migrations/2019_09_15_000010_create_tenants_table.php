b0bebc8939a26157b7d453c5458a7959cad4b725
Tanvir Bhuiyan <tanvirbhuiyann@gmail.com>
2023-11-07 01:47:11 +0600

    Stancl\Tenancy package installed + configured

---
+++ database/migrations/2019_09_15_000010_create_tenants_table.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->string('id')->primary();
+
+            // your custom columns may go here
+            $table->string('name', 255);
+            $table->string('email', 255);
+            $table->string('password', 255);
+
+            $table->timestamps();
+            $table->json('data')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
