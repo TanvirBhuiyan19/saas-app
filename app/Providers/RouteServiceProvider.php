b1b89cbb8f5e43fdbdc86482e038796fa9787149
Tanvir Bhuiyan <tanvirbhuiyann@gmail.com>
2023-11-11 05:56:42 +0600

    api & web routes protect from Tenant

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,13 +28,19 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
-        $this->routes(function () {
-            Route::middleware('api')
+        $centralDomains = config('tenancy.central_domains');
+        
+        $this->routes(function () use ($centralDomains){
+            foreach($centralDomains as $domain){
+                Route::middleware('api')
                 ->prefix('api')
+                ->domain($domain)
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
+                
+                Route::middleware('web')
+                ->domain($domain)
                 ->group(base_path('routes/web.php'));
+            }
         });
     }
 }
